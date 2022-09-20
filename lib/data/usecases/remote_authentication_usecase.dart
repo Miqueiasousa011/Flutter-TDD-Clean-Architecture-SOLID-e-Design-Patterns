0323e05f3c26c414d94042626aa6281b704742ef
Miqueias Pires <miqueiasousa011@gmail.com>
2022-09-19 22:19:43 -0300

    add RemoteAuthenticationParams

--- lib/data/usecases/remote_authentication_usecase.dart
+++ lib/data/usecases/remote_authentication_usecase.dart
@@ -13,13 +13,29 @@ class RemoteAuthenticationUsecase {
         _httpClient = httpClient;
 
   Future auth(AuthenticationParams params) async {
+    final body = RemoteAuthenticationParams.fromDomain(params).toMap();
     return await _httpClient.request(
       url: _url,
       method: 'post',
-      body: {
-        'email': params.email,
-        'password': params.password,
-      },
+      body: body,
     );
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    required this.email,
+    required this.password,
+  });
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(
+        email: params.email,
+        password: params.password,
+      );
+
+  Map<String, dynamic> toMap() => {'email': email, 'password': password};
 }
