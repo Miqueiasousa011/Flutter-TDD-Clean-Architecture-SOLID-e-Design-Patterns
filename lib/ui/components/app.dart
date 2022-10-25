88c0dad748fa678be90534be67eca1155f3ccb9a
Miqueias Pires <miqueiasousa011@gmail.com>
2022-10-25 20:59:46 -0300

    add loginPage layout

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -7,7 +7,27 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
+    final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        primaryColorDark: primaryColorDark,
+        primaryColorLight: primaryColorLight,
+        accentColor: primaryColor,
+        backgroundColor: Colors.white,
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 30,
+            color: primaryColorDark,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      title: '4Dev',
       home: LoginPage(),
     );
   }
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
 
+import '../components/components.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const LoginHeader(),
+            const Headline1(text: 'login'),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(hintText: 'Email'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(hintText: 'Senha'),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Entrar'.toUpperCase(),
+                      ),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.person),
+                      label: const Text('Criar conta'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
