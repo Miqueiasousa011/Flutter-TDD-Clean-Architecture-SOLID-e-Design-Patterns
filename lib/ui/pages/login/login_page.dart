a1e8d44d87cacb0832a55c67b880075a114d83e8
Miqueias Pires <miqueiasousa011@gmail.com>
2022-10-27 22:00:53 -0300

    create loginButton component

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -18,74 +18,61 @@ class _LoginPageState extends State<LoginPage> {
   @override
   void dispose() {
     super.dispose();
-
     widget.loginPresenter!.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Builder(builder: (context) {
-        widget.loginPresenter!.isLoadingController.listen((isLoading) {
-          if (isLoading) {
-            showLoading(context);
-          } else {
-            hidenLoading(context);
-          }
-        });
+      body: Builder(
+        builder: (context) {
+          widget.loginPresenter!.isLoadingController.listen((isLoading) {
+            if (isLoading) {
+              showLoading(context);
+            } else {
+              hidenLoading(context);
+            }
+          });
 
-        widget.loginPresenter!.mainErrorController.listen((error) {
-          if (error != null) {
-            showErrorMessage(context, error);
-          }
-        });
+          widget.loginPresenter!.mainErrorController.listen((error) {
+            if (error != null) {
+              showErrorMessage(context, error);
+            }
+          });
 
-        return SingleChildScrollView(
-          child: Column(
-            children: [
-              const LoginHeader(),
-              const Headline1(text: 'login'),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Provider(
-                  create: (context) => widget.loginPresenter,
-                  child: Form(
-                    child: Column(
-                      children: [
-                        const EmailInput(),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 16),
-                          child: PasswordInput(),
-                        ),
-                        StreamBuilder<bool>(
-                          stream: widget.loginPresenter!.isFormValidController,
-                          builder: (context, snapshot) {
-                            return ElevatedButton(
-                              onPressed: snapshot.data == true
-                                  ? () {
-                                      widget.loginPresenter!.auth();
-                                    }
-                                  : null,
-                              child: Text(
-                                'Entrar'.toUpperCase(),
-                              ),
-                            );
-                          },
-                        ),
-                        TextButton.icon(
-                          onPressed: () {},
-                          icon: const Icon(Icons.person),
-                          label: const Text('Criar conta'),
-                        )
-                      ],
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                const LoginHeader(),
+                const Headline1(text: 'login'),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Provider(
+                    create: (context) => widget.loginPresenter,
+                    child: Form(
+                      child: Column(
+                        children: [
+                          const EmailInput(),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 16),
+                            child: PasswordInput(),
+                          ),
+                          const LoginButton(),
+                          TextButton.icon(
+                            onPressed: () {},
+                            icon: const Icon(Icons.person),
+                            label: const Text('Criar conta'),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        );
-      }),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
