463dfeddfd1dfada2df3346c0243682cedc9ddbb
Miqueias Pires <miqueiasousa011@gmail.com>
2022-10-30 19:30:33 -0300

    ensure RequiredFieldValidtion returns error if value is empty

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -2,18 +2,20 @@ import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 abstract class FieldValidation {
-  String get field;
-
   String? validate(String? value);
 }
 
 class RequiredFieldValidation implements FieldValidation {
-  final String field;
+  final String? field;
 
   RequiredFieldValidation(this.field);
 
   @override
   String? validate(String? value) {
+    if (value == null || value.isEmpty == true) {
+      return 'Campo obrigatório';
+    }
+
     return null;
   }
 }
@@ -32,5 +34,11 @@ void main() {
     final error = sut.validate(value);
 
     expect(error, isNull);
+  });
+
+  test('Should return error if value is empty', () {
+    final error = sut.validate(null);
+
+    expect(error, equals('Campo obrigatório'));
   });
 }
