a0e5d5aed5364a5aa30de023f3f803abe80c4f7d
Miqueias Pires <miqueiasousa011@gmail.com>
2022-10-31 20:57:23 -0300

    ensure ValidationComposite returns the first error of the correct field

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -16,7 +16,7 @@ class ValidationComposite implements Validation {
   String? validate({required String field, required value}) {
     String? error;
 
-    for (var validation in validations) {
+    for (var validation in validations.where((v) => v.field == field)) {
       error = validation.validate(value);
       if (error != null) {
         return error;
@@ -32,18 +32,21 @@ void main() {
   late ValidationComposite sut;
   late MockFieldValidation val1;
   late MockFieldValidation val2;
+  late MockFieldValidation val3;
 
   setUp(() {
     val1 = MockFieldValidation();
     val2 = MockFieldValidation();
+    val3 = MockFieldValidation();
 
-    when(val1.field).thenReturn('any_field');
-    when(val2.field).thenReturn('any_field');
+    when(val1.field).thenReturn('any_field_1');
+    when(val2.field).thenReturn('any_field_2');
+    when(val3.field).thenReturn('any_field_3');
 
     when(val1.validate(any)).thenReturn(null);
     when(val2.validate(any)).thenReturn(null);
 
-    sut = ValidationComposite([val1, val2]);
+    sut = ValidationComposite([val1, val2, val3]);
   });
 
   test('Should return null if all validations returns null', () {
@@ -56,8 +59,17 @@ void main() {
     when(val1.validate(any)).thenReturn('error_1');
     when(val2.validate(any)).thenReturn('error_2');
 
-    final error = sut.validate(field: 'any_field', value: 'any_value');
+    final error = sut.validate(field: 'any_field_2', value: 'any_value');
 
-    expect(error, 'error_1');
+    expect(error, 'error_2');
+  });
+
+  test('Should return error of corresponding field', () {
+    when(val1.validate(any)).thenReturn('error_1');
+    when(val2.validate(any)).thenReturn('error_2');
+    when(val3.validate(any)).thenReturn('other_error');
+    final error = sut.validate(field: 'any_field_3', value: 'any_value');
+
+    expect(error, 'other_error');
   });
 }
