73510477338d875fd34b70874e2676cda15796de
Miqueias Pires <miqueiasousa011@gmail.com>
2022-11-06 15:25:20 -0300

    ensure LocalStorageAdapter throw if save secure throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -24,18 +24,31 @@ class LocalStorageAdapter implements SaveSegureCacheStorage {
 void main() {
   late LocalStorageAdapter sut;
   late MockFlutterSecureStorage secureStorage;
+  late String key;
+  late String value;
 
   setUp(() {
+    key = faker.lorem.word();
+    value = faker.guid.guid();
+
     secureStorage = MockFlutterSecureStorage();
     sut = LocalStorageAdapter(secureStorage: secureStorage);
   });
 
   test('Should call save secure with correct values', () async {
-    const key = 'token';
-    final value = faker.guid.guid();
-
     await sut.save(key: key, value: value);
 
     verify(secureStorage.write(key: key, value: value));
+  });
+
+  test('Should throw if save secure throws', () {
+    when(secureStorage.write(key: key, value: value)).thenThrow(Exception());
+
+    final future = sut.save(key: key, value: value);
+
+    ///Significa que o adapter vai lançar uma exception qualquer
+    ///Não vamos trata-la no adapter. assim não é necessário usar o bloco
+    ///try catch
+    expect(future, throwsA(const TypeMatcher<Exception>()));
   });
 }
