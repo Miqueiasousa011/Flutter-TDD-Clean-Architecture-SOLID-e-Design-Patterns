bb452da9717fc353ec4de953012fd4ca4b7cf324
Miqueias Pires <miqueiasousa011@gmail.com>
2022-11-10 15:20:25 -0300

    ensure RemoteAddAccount throws UnexpectedError on 404

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -66,4 +66,16 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if httpClient returns 404 ', () async {
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenThrow(HttpError.notFound);
+
+    final future = sut.add(addAccountParams);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
