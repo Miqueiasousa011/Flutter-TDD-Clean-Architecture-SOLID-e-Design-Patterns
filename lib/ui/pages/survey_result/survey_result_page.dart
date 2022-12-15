854ecd92f65ab7b9ec2543c78c0f26eb29d67bd6
Miqueias Pires <miqueiasousa011@gmail.com>
2022-12-15 09:47:52 -0300

    add survey result layout

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fordev/utils/i18n/i18n.dart';
 
 class SurveyResultPage extends StatefulWidget {
   const SurveyResultPage({super.key});
@@ -10,6 +11,42 @@ class SurveyResultPage extends StatefulWidget {
 class _SurveyResultPageState extends State<SurveyResultPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(R.strings.surveys),
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.only(top: 20),
+        itemCount: 2,
+        separatorBuilder: (context, index) =>
+            index == 0 ? const SizedBox() : const Divider(),
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                'Qual é o seu ola mundo?',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            );
+          }
+          return Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Ract',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Icon(
+                Icons.check_circle,
+                color: Colors.grey[400],
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
