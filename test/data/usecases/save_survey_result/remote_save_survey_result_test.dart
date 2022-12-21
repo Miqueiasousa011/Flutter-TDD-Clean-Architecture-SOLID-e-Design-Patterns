d8c7540137d1b0474a215a5621c0caeea1f91f9b
Miqueias Pires <miqueiasousa011@gmail.com>
2022-12-21 15:06:25 -0300

    ensure RemoteSaveSurveyResult throws DomainError if HttpClient throws error

--- test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
+++ test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
@@ -1,5 +1,7 @@
 import 'package:faker/faker.dart';
 import 'package:fordev/data/http/http.dart';
+import 'package:fordev/data/models/remote_survey_result_model.dart';
+import 'package:fordev/domain/helpers/helpers.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -17,11 +19,18 @@ class RemoteSaveSurveyResult {
         _url = url;
 
   Future save({required String answer}) async {
-    await _httpClient.request(
-      url: _url,
-      method: 'put',
-      body: {'answer': answer},
-    );
+    try {
+      final response = await _httpClient.request(
+        url: _url,
+        method: 'put',
+        body: {'answer': answer},
+      );
+      RemoteSurveyResultModel.fromJson(response);
+    } on HttpError catch (error) {
+      throw error == HttpError.forbiddenError
+          ? DomainError.accessDenied
+          : DomainError.unexpected;
+    }
   }
 }
 
@@ -33,20 +42,37 @@ void main() {
   late String url;
   late Map<String, dynamic> body;
 
+  late Map<String, dynamic> response;
+
   setUp(() {
     body = {'answer': 'any_answer'};
     url = faker.internet.httpUrl();
     httpClient = MockHttpClient();
     sut = RemoteSaveSurveyResult(httpClient: httpClient, url: url);
-  });
 
-  test('Should call HttpClient with correct values', () async {
+    response = {
+      'surveyId': faker.guid.guid(),
+      'question': faker.randomGenerator.string(50),
+      'answers': [
+        {
+          'image': faker.internet.httpUrl(),
+          'answer': faker.randomGenerator.string(110),
+          'count': faker.randomGenerator.integer(50),
+          'percent': faker.randomGenerator.integer(100),
+          'isCurrentAccountAnswer': faker.randomGenerator.boolean(),
+        }
+      ],
+      'date': faker.date.dateTime().toIso8601String(),
+    };
+
     when(httpClient.request(
             url: anyNamed('url'),
             method: anyNamed('method'),
             body: anyNamed('body')))
-        .thenAnswer((_) async => 'any');
+        .thenAnswer((_) async => response);
+  });
 
+  test('Should call HttpClient with correct values', () async {
     await sut.save(answer: 'any_answer');
 
     verify(httpClient.request(
@@ -54,5 +80,68 @@ void main() {
       method: 'put',
       body: body,
     )).called(1);
+  });
+
+  test(
+      'Should throw UnexpectedError if HttpClient returns 200 with invalid data',
+      () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenAnswer((_) async => {
+              'surveyId': faker.guid.guid(),
+              'questions': faker.randomGenerator.string(50),
+              'answer': [
+                {
+                  'image': faker.internet.httpUrl(),
+                  'answer': faker.randomGenerator.string(110),
+                  'count': faker.randomGenerator.integer(50),
+                  'percent': faker.randomGenerator.integer(100),
+                  'isCurrentAccountAnswer': faker.randomGenerator.boolean(),
+                }
+              ],
+              'date': faker.date.dateTime().toIso8601String(),
+            });
+
+    final future = sut.save(answer: 'any_answer');
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpextedError if HttpClient returns 404', () {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
+
+    final future = sut.save(answer: 'any_answer');
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 500', () {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.serverError);
+
+    final future = sut.save(answer: 'any_answer');
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw  AcessDenidError if HttpClient returns 403', () {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.forbiddenError);
+
+    final future = sut.save(answer: 'any_answer');
+
+    expect(future, throwsA(DomainError.accessDenied));
   });
 }
