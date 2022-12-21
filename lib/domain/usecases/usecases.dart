c6960d5e35bab53aa0cd1779a1a5c01aff1abf38
Miqueias Pires <miqueiasousa011@gmail.com>
2022-12-21 13:35:45 -0300

    add SurveyResultUsecase

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -4,3 +4,4 @@ export './load_current_account_usecase.dart';
 export './add_account_usecase.dart';
 export './load_surveys_usecase.dart';
 export './load_survey_result_usecase.dart';
+export './save_survey_result.dart';
