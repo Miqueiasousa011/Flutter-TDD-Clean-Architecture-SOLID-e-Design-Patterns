d352275e59a37a4761134f2e330cb53789066466
Miqueias Pires <miqueiasousa011@gmail.com>
2022-12-21 11:40:27 -0300

    add RemoteLoadSurveyResultWithLocalFallback factory

--- lib/main/factories/pages/survey_result/survey_result_page_factory.dart
+++ lib/main/factories/pages/survey_result/survey_result_page_factory.dart
@@ -3,9 +3,12 @@ import 'package:fordev/data/usecases/usecases.dart';
 import 'package:fordev/main/factories/http/api_url_factory.dart';
 import 'package:fordev/main/factories/http/http_factory.dart';
 import 'package:get/get.dart';
+import 'package:localstorage/localstorage.dart';
 
+import '../../../../infra/cache/cache.dart';
 import '../../../../presentation/presenters/presenters.dart';
 import '../../../../ui/pages/pages.dart';
+import '../../../composites/composites.dart';
 
 Widget makeSurveyResultPage() {
   return SurveyResultPage(presenter: makeGetxSurveyResultPresenter());
@@ -20,8 +23,15 @@ SurveyResultPresenter makeGetxSurveyResultPresenter() {
 }
 
 makeRemoteLoadSurveyResult(String surveyId) {
-  return RemoteLoadSurveyResult(
-    url: makeApiUrl('/surveys/$surveyId/results'),
-    client: makeHttpAdapter(),
+  return RemoteLoadSurveyResultWithLocalFallback(
+    remoteLoadSurveyResult: RemoteLoadSurveyResult(
+      url: makeApiUrl('/surveys/$surveyId/results'),
+      client: makeHttpAdapter(),
+    ),
+    localLoadSurveyResult: LocalLoadSurveyResult(
+      cacheStorage: LocalStorageAdapter(
+        localStorage: LocalStorage('forDev'),
+      ),
+    ),
   );
 }
